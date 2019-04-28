211c3d8d3fc999af3e6d1a6b066ec3751b60a00f
mihailgaberov <mihail.gaberov@gmail.com>
2019-04-28 10:01:29 +0200

    Initial commit.

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const fileSystem = require('./fileSystem');
+const userInterface = require('./userInterface');
+const search = require('./search');
+
+function main() {
+  userInterface.bindDocument(window);
+  const folderPath = fileSystem.getUsersHomeFolder();
+  userInterface.loadDirectory(folderPath)(window);
+
+  userInterface.bindSearchField((event) => {
+    const query = event.target.value;
+
+    if (query === '') {
+      userInterface.resetFilter();
+    } else {
+      search.find(query, userInterface.filterResults);
+    }
+  });
+}
+
+window.onload = main;
---
+++ search.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const lunr = require('lunr');
+let index = {};
+
+function resetIndex() {
+  index = lunr(function () {
+    this.field('file');
+    this.field('type');
+    this.ref('path');
+  });
+}
+
+function addToIndex(file) {
+  index = lunr(function () {
+    this.add(file);
+  });
+}
+
+function find(query, cb) {
+  if (!index) {
+    resetIndex();
+  }
+
+  const results = index.search(query);
+  cb(results);
+}
+
+module.exports = { addToIndex, find, resetIndex };
